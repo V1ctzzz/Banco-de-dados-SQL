6b8bc3b4a846b84949057239d43da2ba72d3d9cc
Victor Hugo dos Santos Souza <victorhugoplay11@gmail.com>
2023-06-19 23:42:39 -0300

    Add files via upload

---
+++ cadastro.sql
@@ -0,0 +1,6 @@
+insert into pessoas
+(nome, nascimento, sexo, peso, altura, nacionalidade)
+values
+('Victor', '2005-02-07', 'M', '78.5', '1.75', 'brasil'),
+('nicole', '2004-05-13', 'F', '64.2', '1.55', 'italia');
+select * from pessoas;
